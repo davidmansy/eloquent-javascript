b3a6b5de7018657cf0f5bbe6331ffdd2c1a95731
David Mansy <david.mansy@me.com>
2013-08-15 12:34:00 +0200

    Chapter4 updated

--- chapter4.js
+++ chapter4.js
@@ -74,244 +74,109 @@ function chapter4() {
 	//Write a function extractDate that takes such a paragraph as its argument, extracts the date, and returns it as a date object.
 	print("chapter4", 'Exercice 4.6: Write a function extractDate that takes such a paragraph as its argument, extracts the date, and returns it as a date object.');
 	function extractDate(paragraph) {
-		var month = Number(paragraph.slice(5, 7) - 1);
-		var day = Number(paragraph.slice(8, 10));
-		var year = Number(paragraph.slice(11, 15));
-		console.log(year);
-		return new Date(year, month, day);
+		function numberAt(start, length) {
+			return Number(paragraph.slice(start, start + length));
+		}
+		return new Date(numberAt(11, 4), numberAt(8, 2) - 1, numberAt(5, 2));
 	}
 	print("chapter4", "I process the paragraph: 'born 05/04/2006 (mother Lady Penelope): Red Lion, Doctor Hobbles the 3rd, Little Iroquois'")
 	print("chapter4", "I get the following date: " + extractDate(paragraph));
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//Eloquent JavaScript, Exercice 4.1
-document.write("<h1>Exercice 4.1</h1>")
-document.write("<br/>");
-
-var set = {
-	"Spot": true,
-	"Kitty": true
-}
-document.write(set);
-
-set["Romeo"] = true;
-delete set["Kitty"];
-set["Bolo"] = true;
-document.write(set);
-document.write("<br/>");
-document.write("Asoka" in set);
-document.write("<br/>");
-
-//Eloquent JavaScript, Exercice 4.2
-document.write("<h1>Exercice 4.2</h1>")
-document.write("<br/>");
-
-function range(number) {
-	var result = [];
-
-	for (var i = 0; i <= number; i++) {
-		result.push(i);
-	};
-
-	return result;
-}
-document.write(range(24));
-
-//Eloquent JavaScript, Exercice 4.3
-document.write("<h1>Exercice 4.3</h1>")
-document.write("<br/>");
-
-var test = ["Hello", "this is", "terrible"];
-document.write(test.join(" ").split(" "));
-
-
-//Eloquent JavaScript, Exercice 4.4
-document.write("<h1>Exercice 4.4</h1>")
-document.write("<br/>");
-
-function startsWith(string, pattern) {
-	return string.slice(0, pattern.length) == pattern;
-}
-
-document.write(startsWith("This is it!", "This"));
-
-//Eloquent JavaScript, Exercice 4.5
-document.write("<h1>Exercice 4.5</h1>")
-document.write("<br/>");
-var paragraph = "born 15-11-2003 (mother Spot): White Fang, Noog";
-
-function catNames(text) {
-	var names = text.slice(text.indexOf(":") + 2).split(", ");
-	return names;
-}
-
-document.write(catNames(paragraph));
-
-//Eloquent JavaScript, Exercice 4.6
-document.write("<h1>Exercice 4.6</h1>")
-document.write("<br/>");
-
-var para = "died 27/04/2006: Black Leclère";
-
-function extractDate(text) {
-
-	function numberAt(start, length) {
-		return Number(text.slice(start, start+length))
+	printbr("chapter4");
+
+	//Eloquent JavaScript, Exercice 4.7
+	//Write a function between that takes three arguments, all of which are strings. It will return the part of the first argument that occurs between the patterns given by the second and the third arguments.
+	print("chapter4", 'Exercice 4.7: Write a function between that takes three arguments, all of which are strings. It will return the part of the first argument that occurs between the patterns given by the second and the third arguments.');
+
+	function between(string, left, right) {
+		var start = string.indexOf(left) + right.length;
+		var end = string.indexOf(right, start);
+		return string.slice(start, end);
 	}
-	return new Date(numberAt(11,4), numberAt(8,2) - 1, numberAt(5,2));
-}
+	print("chapter4", between("born 15/11/2003 (mother Spot): White Fang", "(mother ", ")"));
+	print("chapter4", between("bu ] boo [ bah ] gzz", "[ ", " ]"));
+	printbr("chapter4");
 
-document.write(extractDate(para));
-
-//Eloquent JavaScript, Exercice 4.7
-document.write("<h1>Exercice 4.7</h1>")
-document.write("<br/>");
-
-function between(text, left, right) {
-	var start = text.indexOf(left) + left.length;
-	var end = text.indexOf(right, start);
-	return text.slice(start, end)
-}
-document.write(between("born 15/11/2003 (mother Spot): White Fang", "(mother ", ")"));
-document.write("<br/>");
-document.write(between("bu ] boo [ bah ] gzz", "[ ", " ]"));
-
-//Eloquent JavaScript, Exercice 4.8
-document.write("<h1>Exercice 4.8</h1>")
-document.write("<br/>");
-
-var today = new Date();
-
-function formatDate(date) {
-	function pad(number) {
-		if (number < 10)
-			return "0" + number;
-		else
-			return number;
-	}
-
-	return pad(date.getDate()) + "/" + pad(date.getMonth() + 1) + "/" + date.getFullYear();
-}
-
-document.write(formatDate(today));
-
-//Eloquent JavaScript, Exercice 4.9
-document.write("<h1>Exercice 4.9</h1>")
-document.write("<br/>");
-
-
-function Cat(name, date) {
-	this.name = name;
-	this.birthDate = date;
-}
-
-var vali = new Cat("Vali", new Date(2007, 9, 1));
-var romy = new Cat("Romy", new Date(1975, 7, 8));
-var david = new Cat("David", new Date(1973, 4, 5));
-
-//var cats = [vali, romy, david];
-var cats = {};
-cats["Vali"] = vali;
-cats["Romy"] = romy;
-cats["David"] = david;
-cats["David"].death = true;
-
-function oldestCat(catContainerObject) {
-	var oldest = null;
-	for(var name in catContainerObject) {
-		var cat = catContainerObject[name];
-		if(!("death" in cat) && (oldest == null || cat.birthDate < oldest.birthDate)) {
-			oldest = cat;
+	//Eloquent JavaScript, Exercice 4.8
+	//The formatDate function used by catInfo does not add a zero before the month and the day part when these are only one digit long. Write a new version that does this.
+	print("chapter4", 'Exercice 4.8: The formatDate function used by catInfo does not add a zero before the month and the day part when these are only one digit long. Write a new version that does this.');
+	function formatDate(date) {
+		function addZero(num) {
+			return num < 10 ? '0' + num : num;
 		}
+		return addZero(date.getDate()) + '/' + addZero(date.getMonth() + 1) + '/' + date.getFullYear();
 	}
-	if (oldest == null)
-		return null;
-	else
-		return oldest.name;
-}
+	var today = new Date();
+	print("chapter4", "Today is " + formatDate(today));
+	printbr("chapter4");
 
-document.write(oldestCat(cats));
-
-//Eloquent JavaScript, Exercice 4.10
-document.write("<h1>Exercice 4.10</h1>")
-document.write("<br/>");
-
-function rangeUpgrade(start, end) {
-	var result = [];
-
-	if (arguments.length < 2) {
-		end = start;
-		start = 0;
+	//Eloquent JavaScript, Exercice 4.9
+	//Write a function oldestCat which, given an object containing cats as its argument, returns the name of the oldest living cat.
+	print("chapter4", 'Exercice 4.9: Write a function oldestCat which, given an object containing cats as its argument, returns the name of the oldest living cat.');
+	function Cat(name, birthDate, deathDate) {
+		this.name = name;
+		this.birthDate = birthDate;
+		if(arguments.length == 3)
+			this.death = deathDate;
 	}
 
-	for (var i = start; i <= end; i++) {
-		result.push(i);
+	var vali = new Cat("Vali", new Date(2007, 9, 1));
+	var romy = new Cat("Romy", new Date(1975, 7, 8));
+	var david = new Cat("David", new Date(1973, 4, 5));
+	var john = new Cat("John", new Date(1963, 4, 5), new Date(2009, 1, 1));
+
+	//var cats = [vali, romy, david];
+	var cats = {};
+	cats["Vali"] = vali;
+	cats["Romy"] = romy;
+	cats["David"] = david;
+
+	function oldestCat(cats) {
+		var oldestCat = {};
+		for (var cat in cats) {
+			if (!("death" in cats[cat])  && (oldestCat.name == undefined || cats[cat].birthDate < oldestCat.birthDate)) {
+				oldestCat.name = cats[cat].name;
+				oldestCat.birthDate = cats[cat].birthDate;
+			}
+		}
+		if (oldestCat.name == undefined)
+			return null;
+		else
+			return oldestCat.name; 
 	}
-	return result;
-}
+	print("chapter4", "The oldest cat is named: " + oldestCat(cats));
+	printbr("chapter4");
 
-function sum(result) {
-	var total = 0;
-	for (var i = 0; i < result.length; i++) {
-		total += result[i];
+	//Eloquent JavaScript, Exercice 4.10
+	//Extend the range function from exercise 4.2 to take a second, optional argument. If two arguments are given, the first indicates the start of the range, the second the end.
+	print("chapter4", 'Exercice 4.10: Extend the range function from exercise 4.2 to take a second, optional argument. If two arguments are given, the first indicates the start of the range, the second the end');
+	function anotherRange(target, start) {
+
+		if (arguments.length == 1)
+			start = 0;
+
+		var result = [];
+		for (var i = start; i <= target; i++) {
+			result.push(i);
+		};
+		return result;
 	}
-	return total;
-}
+	print("chapter4", "Range 12->24:  " + anotherRange(24, 12));
+	print("chapter4", "Range 0->12 :  " + anotherRange(12));
+	printbr("chapter4");
 
-document.write(sum(rangeUpgrade(24)));
-document.write("<br/>");
-document.write(sum(rangeUpgrade(10,24)));
-*/
+	//Eloquent JavaScript, Exercice 4.11
+	//This function takes an array of numbers, and returns their sum. Write it, it should be easy.
+	print("chapter4", 'Exercice 4.11: This function takes an array of numbers, and returns their sum. Write it, it should be easy.');
+	function sum(array) {
+		var sum = 0;
+		for (var i = 0; i < array.length; i++) {
+			sum += array[i];
+		};
+		return sum;
+	}
 
+	print("chapter4", "Sum of the Range 12->24:  " + sum(anotherRange(24, 12)));
+	print("chapter4", "Sum of the Range 0->12 :  " + sum(anotherRange(12)));
+	printbr("chapter4");
 
-
-
-
-
-
-
+}}
